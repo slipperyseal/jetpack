64b542f7c8ad722e5e0018c5cf53063bf7aaddf0
Christian Catchpole <christian@catchpole.net>
2022-12-29 09:39:04 +1100

    formatting

--- internal/app/jetpack/sid.go
+++ internal/app/jetpack/sid.go
@@ -8,27 +8,27 @@ import (
 )
 
 type SidHeader struct {
-	MagicID uint32 		   // 0
-	Version uint16		   // 4
-	DataOffset uint16	   // 6
-	LoadAddress uint16     // 8
-	InitAddress uint16     // A
-	PlayAddress uint16     // c
-	Songs uint16		   // e
-	StartSong uint16	   // 10
-	Speed uint32		   // 12
-	Name [32]byte	       // 16
-	Author [32]byte        // 36
-	Released [32]byte      // 56
-	Flags uint16		   // 76
-	StartPage uint8  	   // 78
-	PageLength uint8       // 79
-	SecondSIDAddress uint8 // 7a
-	ThirdSIDAddress uint8  // 7b
-	FirstData uint16       // 7c
+	MagicID          uint32   // 0
+	Version          uint16   // 4
+	DataOffset       uint16   // 6
+	LoadAddress      uint16   // 8
+	InitAddress      uint16   // A
+	PlayAddress      uint16   // c
+	Songs            uint16   // e
+	StartSong        uint16   // 10
+	Speed            uint32   // 12
+	Name             [32]byte // 16
+	Author           [32]byte // 36
+	Released         [32]byte // 56
+	Flags            uint16   // 76
+	StartPage        uint8    // 78
+	PageLength       uint8    // 79
+	SecondSIDAddress uint8    // 7a
+	ThirdSIDAddress  uint8    // 7b
+	FirstData        uint16   // 7c
 }
 
-func readSidHeader(file *os.File)  {
+func readSidHeader(file *os.File) {
 	binary.Read(file, binary.BigEndian, &sidHeader.MagicID)
 	binary.Read(file, binary.BigEndian, &sidHeader.Version)
 	binary.Read(file, binary.BigEndian, &sidHeader.DataOffset)
@@ -72,7 +72,7 @@ func writeSidInfo(path string) {
 // for use with Monty Stereo SID Synth
 // https://github.com/slipperyseal/monty
 // sid_init() and sid_play() are expected to be called from an ISR where avr-gcc has saved registers
-func writeMontyFunctions()  {
+func writeMontyFunctions() {
 	fmt.Printf(".global sid_init\n")
 	fmt.Printf("    .type sid_init, @function\n")
 	fmt.Printf("sid_init:\n")
@@ -91,11 +91,11 @@ func writeMontyFunctions()  {
 	fmt.Printf("    clr %s\n", REGZ)
 	fmt.Printf("    rjmp L%04x\n\n", flattenJumpAddress(sidHeader.PlayAddress))
 
-	fmt.Printf("sid_write:\n")				// REGU = address, REGT = value
-	fmt.Printf("    out 0x5,%s\n", REGT)     // PORTB 7 segment LED
-	fmt.Printf("    out 0x8,%s\n", REGT)     // PORTC = val
+	fmt.Printf("sid_write:\n")           // REGU = address, REGT = value
+	fmt.Printf("    out 0x5,%s\n", REGT) // PORTB 7 segment LED
+	fmt.Printf("    out 0x8,%s\n", REGT) // PORTC = val
 	fmt.Printf("    andi %s, 0x1f\n", REGU)
-	fmt.Printf("    out 0x2,%s\n", REGU)     // PORTA = reg | sidSelect; (0 = both)
+	fmt.Printf("    out 0x2,%s\n", REGU) // PORTA = reg | sidSelect; (0 = both)
 	for nop := 0; nop < 16; nop++ {
 		fmt.Printf("    nop\n")
 	}
