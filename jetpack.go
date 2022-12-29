2929ee543feac8564b94a954a98bba84716aea09
Christian Catchpole <christian@catchpole.net>
2022-12-30 10:19:47 +1100

    fix args defaults

--- jetpack.go
+++ jetpack.go
@@ -21,7 +21,7 @@ func sidArg() string {
 func arg(option string, dephault bool) bool {
 	for _, v := range os.Args[1:] {
 		if v == option {
-			return true
+			return !dephault
 		}
 	}
 
@@ -30,9 +30,9 @@ func arg(option string, dephault bool) bool {
 
 func main() {
 	jetpack.BlastOff(sidArg(),
-		arg("-failonselfmodify", true), // fail if we notice writes into the code space
-		arg("-printalllabels", false),  // print labels for every instruction, not just jump and branch points
-		arg("-writesidbinary", false),  // write the sid binary which can be loaded into 6502 tools
-		arg("-writememorymap", true),   // write the memory map as a png which can be looked at with your eyeballs
-		0)                              // try using the lower value of "RAM access range" reported in the output source
+		arg("-allowselfmodify", true), // fail if we notice writes into the code space
+		arg("-printalllabels", false), // print labels for every instruction, not just jump and branch points
+		arg("-writesidbinary", false), // write the sid binary which can be loaded into 6502 tools
+		arg("-nomemorymap", true),     // write the memory map as a png which can be looked at with your eyeballs
+		0)                             // try using the lower value of "RAM access range" reported in the output source
 }
